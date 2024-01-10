cd4252abe51b1ee80d41f28abb5a2187e5584ce5
ABHINAV AGARWAL <68347344+abhinav-mangal@users.noreply.github.com>
2024-01-10 17:20:54 +0530

    update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\91963\Documents\linker\Millat"
+export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/3.13.2/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/abhinavagarwal/Documents/Work/millat"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=/Users/abhinavagarwal/Documents/Work/millat/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=3.1.2"
 export "FLUTTER_BUILD_NUMBER=30"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iMjAxODNlMDQwOTYwOTRiY2MzN2Q5Y2RlMmE0Yjk2ZjVjYzY4NGNmLw=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/abhinavagarwal/Documents/Work/millat/.dart_tool/package_config.json"
